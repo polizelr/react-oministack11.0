9a35205492694946d37735d1eba6b3ac909f2df9
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-05-29 23:59:21 -0300

    database configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,13 @@
 //import express
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
-    return res.json({
-        evento: 'Semana OmniStack 11.0',
-        aluna: 'Rafaela Polizel'
-    });
-});
+
 
 app.listen(3333);
 
